436f512de1cdf2ff4cc630137cd2a0c4f6f2e62c
João Lopes <35151466+joaovslopes@users.noreply.github.com>
2024-06-16 01:25:33 -0300

    nodemon and string mongodb

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
 const app = express();
 const PORT = 8001;
 
+connectToMongoDB('mongodb+srv://joaovslopesz:l25lD10A01Rhjofr@bancoshortlink.iptf7z8.mongodb.net/?retryWrites=true&w=majority&appName=BancoShortLink').then(() => 
+    console.log('Mongodb connected')
+);
 
 app.use("/url", urlRoute);
 
